33aa7f21a115c5e9707ebf1acddf75067fbac105
artemsyvko <artemsyvko71@gmail.com>
2023-08-17 03:52:34 +0300

    created view
    columns: book_id, title, year, author, reviewer_id, reviewer_name, rating, rating_date

--- main.sql
+++ main.sql
@@ -1,4 +1,11 @@
 -- 
+-- CREATE DATABASE
+-- 
+DROP DATABASE bookstore;
+CREATE DATABASE bookstore;
+USE bookstore;
+
+-- 
 -- CREATE TABLES
 -- 
 CREATE TABLE books(id INT, title VARCHAR(250), year INT, author VARCHAR(250));
@@ -6,3 +13,23 @@ CREATE TABLE books(id INT, title VARCHAR(250), year INT, author VARCHAR(250));
 CREATE TABLE reviewers(id INT, name VARCHAR(250));
 
 CREATE TABLE ratings(reviewer_id INT, book_id INT, rating INT, rating_date DATE);
+
+-- 
+-- CREATING VIEWS
+-- 
+CREATE VIEW book_reviewer_rating AS
+SELECT
+    bo.id,
+    bo.title,
+    bo.year,
+    bo.author,
+    ra.reviewer_id,
+    re.name AS reviewer,
+    ra.rating,
+    ra.rating_date
+FROM books AS bo
+JOIN
+    ratings AS ra ON bo.id = ra.book_id
+JOIN
+    reviewers AS re ON ra.reviewer_id = re.id
+    ;
